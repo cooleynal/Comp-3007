66e193a856e9851f5e185bb8e55bb5bed1673f6d
nick cooley <cooleynal@gmail.com>
2024-10-10 20:19:46 -0400

    111

--- a/a6 SUPER EXP2.hs
+++ a/a6 SUPER EXP2.hs
@@ -19,7 +19,6 @@ parseVar str = do
     return (PR (Var name) rest)
 
 
-
 parseOpName :: Parser String
 parseOpName str = do
     PR name rest <- parseName str
@@ -30,39 +29,37 @@ parseOpName str = do
 parseIf :: Parser Exp
 parseIf str = do
     PR opName rest1 <- parseOpName str
-    if opName /= "if"
-        then Nothing
-        else do
-            PR _ rest2 <- parseChar '(' rest1
-            PR cond rest3 <- parseExp rest2
-            PR _ rest4 <- parseChar ',' rest3
-            PR thenExp rest5 <- parseExp rest4
-            PR _ rest6 <- parseChar ',' rest5
-            PR elseExp rest7 <- parseExp rest6
-            PR _ finalRest <- parseChar ')' rest7
-            return $ PR (If cond thenExp elseExp) finalRest
+    -- failIf (opName /= "if")
+    PR _ rest2 <- parseChar '(' rest1
+    PR cond rest3 <- parseExp rest2
+    PR _ rest4 <- parseChar ',' rest3
+    PR thenExp rest5 <- parseExp rest4
+    PR _ rest6 <- parseChar ',' rest5
+    PR elseExp rest7 <- parseExp rest6
+    PR _ rest8 <- parseChar ')' rest7
+    return $ PR (If cond thenExp elseExp) rest8
+
 
 parseApp2 :: Parser Exp
 parseApp2 str = do
     PR funcName rest1 <- parseOpName str
+    -- failIf (funcName == "if")
     PR _ rest2 <- parseChar '(' rest1
     PR arg1 rest3 <- parseExp rest2
     PR _ rest4 <- parseChar ',' rest3
     PR arg2 rest5 <- parseExp rest4
     PR _ rest6 <- parseChar ')' rest5
-    if null funcName
-        then Nothing
-        else return $ PR (App2 funcName arg1 arg2) rest6
+    return $ PR (App2 funcName arg1 arg2) rest6
 
 
 parseApp1 :: Parser Exp
 parseApp1 str = do
     PR funcName rest1 <- parseName str
+    -- failIf (funcName == "if")
     PR _ rest2 <- parseChar '(' rest1
     PR arg rest3 <- parseExp rest2
-    -- PR arg rest3 <- parseVar rest2
-    PR _ finalRest <- parseChar ')' rest3
-    return $ PR (App1 funcName arg) finalRest
+    PR _ rest4 <- parseChar ')' rest3
+    return $ PR (App1 funcName arg) rest4
 
 parseDef :: Parser Def
 parseDef str = do
@@ -70,17 +67,19 @@ parseDef str = do
     PR _ rest2 <- parseChar '=' rest1
     PR exp rest3 <- parseExp rest2
     parseEnd rest3
-    if null name
-      then Nothing
-      else return $ PR (Def name exp) rest3
+    return $ PR (Def name exp) rest3
 
 main :: IO ()
 main = do
+
+    let testInput = "if(stuff(x),y,z)"
+    let result = parseIf testInput
+    print result
+    putStrLn ""
+
     contents <- readFile "program.txt"
     putStrLn "Original contents:"
     putStrLn contents
-
-    -- putStrLn parseOpName "w(x)"
 
     let linesOfFile = lines contents
     let trimmedLines = map (filter (/= ' ')) linesOfFile
--- a/a6.hs
+++ a/a6.hs
@@ -28,7 +28,7 @@
 
 
 
-module A6 where
+-- module A6 where
 import Debug.Trace (trace)
 import Prelude hiding (head, tail)
 
@@ -154,44 +154,105 @@ parseDigits = collect isDigit
 -- side should be lines in the `do`.
 ------------------------------------------------------------------------------
 
--- parseExp :: Parser Exp
--- parseExp = parseConst +++ parseVar +++ parseIf +++ parseApp1 +++ parseApp2
+-- import A6
 
--- parseConst :: Parser Exp
--- parseConst str = do
---   undefined
+parseExp :: Parser Exp
+parseExp = parseConst +++ parseVar +++ parseIf +++ parseApp1 +++ parseApp2
 
--- parseVar :: Parser Exp
--- parseVar str = do
---   undefined
+parseConst :: Parser Exp
+parseConst str = do
+    PR intPart restAfterDigits <- parseDigits str
+    PR _ restAfterDecimal <- parseChar '.' restAfterDigits
+    PR fracPart restAfterFrac <- parseDigits restAfterDecimal
+    let numberString = intPart ++ "." ++ fracPart
+    let number = read numberString :: Double
+    return $ PR (Const number) restAfterFrac
 
--- parseOpName :: Parser String
--- parseOpName str = do
---   undefined
+parseVar :: Parser Exp
+parseVar str = do
+    PR name rest <- parseName str
+    failIf (headIs (== '(') rest)
+    return (PR (Var name) rest)
 
--- parseIf :: Parser Exp
--- parseIf str = do
---   undefined
 
--- parseApp2 :: Parser Exp
--- parseApp2 str = do
---   undefined
+parseOpName :: Parser String
+parseOpName str = do
+    PR name rest <- parseName str
+    failIf (headIs (/= '(') rest)
+    return $ PR name rest
 
--- parseApp1 :: Parser Exp
--- parseApp1 str = do
---   undefined
 
--- parseDef :: Parser Def
--- parseDef str = do
---   undefined
+parseIf :: Parser Exp
+parseIf str = do
+    PR opName rest1 <- parseOpName str
+    -- failIf (opName /= "if")
+    PR _ rest2 <- parseChar '(' rest1
+    PR cond rest3 <- parseExp rest2
+    PR _ rest4 <- parseChar ',' rest3
+    PR thenExp rest5 <- parseExp rest4
+    PR _ rest6 <- parseChar ',' rest5
+    PR elseExp rest7 <- parseExp rest6
+    PR _ rest8 <- parseChar ')' rest7
+    return $ PR (If cond thenExp elseExp) rest8
 
--- printDefs :: [Def] -> IO ()
--- printDefs [] = return ()
--- printDefs (def : defs) = do
---   putStrLn (show def)
---   printDefs defs
 
--- -- Read in a file named program.txt where each line is a string representing a
--- -- Def. Parse each line and use printDefs to printout the parsed Defs.
--- main = do
---   undefined
+parseApp2 :: Parser Exp
+parseApp2 str = do
+    PR funcName rest1 <- parseOpName str
+    -- failIf (funcName == "if")
+    PR _ rest2 <- parseChar '(' rest1
+    PR arg1 rest3 <- parseExp rest2
+    PR _ rest4 <- parseChar ',' rest3
+    PR arg2 rest5 <- parseExp rest4
+    PR _ rest6 <- parseChar ')' rest5
+    return $ PR (App2 funcName arg1 arg2) rest6
+
+
+parseApp1 :: Parser Exp
+parseApp1 str = do
+    PR funcName rest1 <- parseName str
+    -- failIf (funcName == "if")
+    PR _ rest2 <- parseChar '(' rest1
+    PR arg rest3 <- parseExp rest2
+    PR _ rest4 <- parseChar ')' rest3
+    return $ PR (App1 funcName arg) rest4
+
+parseDef :: Parser Def
+parseDef str = do
+    PR name rest1 <- parseName str
+    PR _ rest2 <- parseChar '=' rest1
+    PR exp rest3 <- parseExp rest2
+    parseEnd rest3
+    return $ PR (Def name exp) rest3
+
+main :: IO ()
+main = do
+
+    let testInput = "if(stuff(x),y,z)"
+    let result = parseIf testInput
+    print result
+    putStrLn ""
+
+    contents <- readFile "program.txt"
+    putStrLn "Original contents:"
+    putStrLn contents
+
+    let linesOfFile = lines contents
+    let trimmedLines = map (filter (/= ' ')) linesOfFile
+
+    putStrLn "Trimmed and arrayed:"
+    print trimmedLines
+
+    putStrLn "Parsed definitions:"
+    putStrLn ""
+    processLines trimmedLines
+
+processLines :: [String] -> IO ()
+processLines [] = return ()
+processLines (line:lines) = do
+    putStrLn $ "Parsing line: " ++ line
+    case parseDef line of
+        Just (PR def _) -> do
+            putStrLn (show def)
+        Nothing -> putStrLn ("FAILED " ++ line)
+    processLines lines
